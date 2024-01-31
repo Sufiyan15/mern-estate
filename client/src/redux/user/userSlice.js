5c70799586419a0b950e7bdee46c8bfd5e7e4a52
Sufiyan15 <sufiyanchhipa1519@gmail.com>
2024-02-01 00:27:59 +0530

    Update the Header and make the Profile Page private

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -14,7 +14,7 @@ const userSlice = createSlice({
       state.loading = true;
     },
     signInSuccess: (state, action) => {
-      state.user = action.payload;
+      state.currentUser = action.payload;
       state.loading = false;
       state.error = null;
     },
